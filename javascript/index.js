9a0e2fd994d2851cc9771b44ad372baaecd2e77e
shady003 <saurabhyadav9909@gmail.com>
2022-10-09 13:09:56 +0530

    kjsd
    Folders Change

---
+++ javascript/index.js
@@ -0,0 +1,3 @@
+var qname = prompt("Enter Your userName Comfertably");
+document.getElementById('clint').innerHTML= "<strong>Welcome </strong>" + qname;
+document.getElementById('userName').value = qname ;
